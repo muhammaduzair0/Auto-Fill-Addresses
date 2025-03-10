7ac4ec8dde26bdbda13beb8736cae997425064e8
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-10 16:21:14 +0500

    Initialize DOM elements for address form functionality

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //DOM Elements
+
+    const addressSearch = document.getElementById('addressSearch')
+    const searchBtn = document.getElementById('searchBtn')
+    const AddressSuggestions = document.getElementById('addressSuggestions')
+    const streetAddress = document.getElementById('streetAddress')
+    const city = document.getElementById('city')
+    const state = document.getElementById('state')
+    const zipCode = document.getElementById('zipCode')
+    const addressForm = document.getElementById('addressForm')
+})
